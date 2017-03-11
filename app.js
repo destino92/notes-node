e3ba07e801eecf4678eb7d45b6e67436259259fd
destino92 <destinodello@gmail.com>
2017-03-11 21:36:16 +0300

    feat: added method to fetch a note and display it's content

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
 
   if(note){
-    console.log("Note created", note.title, note.body);
+    console.log("Note created");
     console.log("--");
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
@@ -25,7 +25,16 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+
+  if (note){
+    console.log("Note found");
+    console.log("--");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log("Note not found");
+  }
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
 
